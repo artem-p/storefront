36201cc2e02d729ff4ff2be2f2abe97f9c6bdcac
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-18 15:26:59 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+    return (
+        <div className='header'>
+            <img
+                className='header__logo' 
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+        </div>
+    )
+}
+
+export default Header
