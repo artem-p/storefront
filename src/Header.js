886bc615dd0cd0f2cc7aaee2761427f0f3eb22f3
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-26 12:45:24 +0300

    logo link to home

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,17 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from 'react-router-dom';
 
 
 function Header() {
     return (
         <div className='header'>
-            <img
-                className='header__logo' 
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+            <Link to='/'>
+                <img
+                    className='header__logo' 
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+            </Link>
             
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
