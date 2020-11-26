4a89f50748a42d1c61b6dba7d2c317d13c6c9967
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-26 12:43:16 +0300

    checkout layout

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,8 +4,22 @@ import './Checkout.css';
 
 function Checkout() {
     return (
-        <div>
-            
+        <div className="checkout">
+            <div className="checkout__left">
+                <img
+                    className="checkout__ad" 
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
+                    alt=""
+                />
+
+                <div>
+                    <h2 className="checkout__title">Your Shopping Basket</h2>
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <h2>The subtotal will going here</h2>
+            </div>
         </div>
     )
 }
