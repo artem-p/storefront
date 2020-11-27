093ee0a2f106d06cf00e0564595416c00d31e591
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-27 17:52:54 +0300

    update basket number

--- src/Product.js
+++ src/Product.js
@@ -4,7 +4,7 @@ import { useStateValue } from './StateProvider';
 
 
 function Product({id, title, image, price, rating}) {
-    const [state, dispatch] = useStateValue();
+    const [{ basket }, dispatch] = useStateValue();
 
     const addToBasket = () => {
         // dispatch the item into the data layer
