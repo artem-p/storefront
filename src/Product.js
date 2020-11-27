9bfcc9744f9e1e0edb51c6ee6febd7ab7dda83cc
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-27 17:36:43 +0300

    add to basket dispatch

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 
 function Product({id, title, image, price, rating}) {
+    const [state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            },
+        });
+    }
+
     return (
         <div className="product">
             <div className="product__info">
@@ -22,7 +39,7 @@ function Product({id, title, image, price, rating}) {
             
             <img src={image} alt=""/>
 
-            <button className="product__button">Add to Basket</button>
+            <button onClick={addToBasket} className="product__button">Add to Basket</button>
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ export const initialState = {
 };
 
 const reducer = (state, action) => {
+    console.log(action);
+
     switch(action.type){
         case 'ADD_TO_BASKET':
             return {
